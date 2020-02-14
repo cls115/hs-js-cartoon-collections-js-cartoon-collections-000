028c91f7126eaf882eb3e8aaa22c6177f5f0a6df
Colton stephens <stephens.colton@icloud.com>
2020-02-14 17:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,20 +16,25 @@ function summonCaptainPlanet(planeteerCalls){
 shortWords = ["wind", "fire"];
 assortedWords = ["earth", "wind", "heart", "fire"];
 function longPlaneteerCalls(words) {
-  var result = false
-for(var i = 0; i < words.length; i++) {
-	if(words[i].length > 4) {
-	result = true	}
-}
-return result
+  result = false 
+  for(var i = 0; i < words.length; i++) {
+  	if(words[i].length > 4) {
+  	result = true	}
+  }
+  console.log("result", result);
+  return result
 }
 function findTheCheese (foods) {
   var cheeses = ["cheddar", "gouda", "camembert"];
-  for (var i = 0; i < cheeses.length; i++) {
-     for (var index = 0; index < foods.length; index++){ 
-       if (cheeses[i]=== foods[index])
-       return "cheddar";
-   }
-   return 'no cheese!';
+  for(var i = 0;i < foods.length; i++) {
+  if (cheeses.includes(foods[i])){
+  return (foods[i])
   }
 }
+  return "no cheese!"
+}
+  // for (var i = 0; i < cheeses.length; i++)
+  //   for (var index = 0; index < foods.length; index++){ 
+  //     if (cheeses[i]=== foods[index])
+  //     return "cheddar";
+  // }
